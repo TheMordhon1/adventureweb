6d5e3404a80a521aedee50c4de0e2b83711ff73c
TheMordhon1 <52556786+TheMordhon1@users.noreply.github.com>
2020-02-04 00:26:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Canvas Little Guy
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.moveTo(75, 25);
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 65, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
+
+ctx.fillStyle = 'maroon';
+ctx.fill();
+ctx.font = '1rem Arial';
+ctx.fillStyle = '#fff';
+ctx.fillText('Im Following', 30, 65);
+
+
+$(function () {
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
+    });
+});
